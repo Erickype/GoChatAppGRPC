5ad577cee1dfe04a8b1992f8f9ef3ae9a3b3703e
Erickype <erickype@hotmail.com>
2022-09-23 15:59:10 -0500

    Connection struct for the broadcast server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Erickype/GoChatAppGRPC/proto"
 	log "google.golang.org/grpc/grpclog"
 	"os"
 )
@@ -9,6 +10,13 @@ var grpcLog log.LoggerV2
 
 func init() {
 	grpcLog = log.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
+}
+
+type Connection struct {
+	stream proto.Broadcast_CreateStreamServer
+	id     string
+	active bool
+	error  chan error
 }
 
 func main() {
