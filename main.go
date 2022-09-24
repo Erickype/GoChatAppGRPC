f2f0c9bfe91e72d565a2e0cf0561173c942b93df
Erickype <erickype@hotmail.com>
2022-09-24 16:08:36 -0500

    BroadcastMessage function declared

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"context"
 	"github.com/Erickype/GoChatAppGRPC/proto"
 	log "google.golang.org/grpc/grpclog"
 	"os"
 )
 
-var grpcLog log.LoggerV2
+var _ log.LoggerV2
 
 func init() {
-	grpcLog = log.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
+	_ = log.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
 }
 
 type Connection struct {
@@ -24,16 +25,22 @@ type Server struct {
 	Connection []*Connection
 }
 
-func (s *Server) CreateStream(pconn *proto.Connect, stream proto.Broadcast_CreateStreamServer) error {
+func (s *Server) CreateStream(connect *proto.Connect, stream proto.Broadcast_CreateStreamServer) error {
 	conn := &Connection{
 		stream: stream,
-		id:     pconn.User.Id,
+		id:     connect.User.Id,
 		active: true,
 		error:  make(chan error),
 	}
 
 	s.Connection = append(s.Connection, conn)
 	return <-conn.error
+}
+
+func (s *Server) BroadCastMessage(_ context.Context, _ *proto.Message) (*proto.Close, error) {
+
+	return &proto.Close{}, nil
+
 }
 
 func main() {
