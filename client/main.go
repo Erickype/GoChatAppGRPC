76b9ec27122af9d8e39ae3345f603b2dd0a9403a
Erickype <erickype@hotmail.com>
2022-10-06 11:24:51 -0500

    Connect function that implements createStream of grpc and function request added

--- client/main.go
+++ client/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"context"
 	"github.com/Erickype/GoChatAppGRPC/proto"
+	log "google.golang.org/grpc/grpclog"
 	"sync"
 )
 
-var _ proto.BroadcastClient
+var client proto.BroadcastClient
 var wait *sync.WaitGroup
 
 func init() {
 	wait = &sync.WaitGroup{}
+}
+
+func connect(user *proto.User) error {
+	var streamError error
+
+	_, err := client.CreateStream(context.Background(), &proto.Connect{
+		User:   user,
+		Active: true,
+	})
+	if err != nil {
+		log.Fatalf("Error connecting: %v", err.Error())
+	}
+
+	return streamError
 }
 
 func main() {
